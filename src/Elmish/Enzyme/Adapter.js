3caa4e0f7200c9cf1a61d230b44e31725edb47b4
sigma-andex <sigma.andex@pm.me>
2022-02-12 16:59:33 +0000

    Fix dynamic import

--- src/Elmish/Enzyme/Adapter.js
+++ src/Elmish/Enzyme/Adapter.js
@@ -1,6 +1,8 @@
 export function adapterImpl(pkg) {
   return () => {
     const Adapter = import(pkg)
-    return Adapter.then(a => new a())
+    return Adapter.then(m => {
+      const Adaptr = m.default;
+      return function () { return new Adaptr()}} )
   };
 }
